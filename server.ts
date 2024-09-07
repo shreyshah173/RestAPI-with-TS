42ed4e583cc849cba022fceb7dd8768072e3ee30
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-07 15:46:36 +0530

    JSON core module

--- server.ts
+++ server.ts
@@ -8,24 +8,33 @@ const host = '127.0.0.1';
 
 const server : Server = http.createServer((req,res)=>{
     res.statusCode=200;
-    res.setHeader('Content-Type','text/html');
-    let osData = {
-        totalMemory : os.totalmem(),
-        freeMemory : os.freemem(),
-        homedir : os.homedir(),
-        computerName : os.hostname()
-    };
+    // res.setHeader('Content-Type','text/html');
+    res.setHeader('Content-Type','application/json');
+    // let osData = {
+    //     totalMemory : os.totalmem(),
+    //     freeMemory : os.freemem(),
+    //     homedir : os.homedir(),
+    //     computerName : os.hostname()
+    // };
 
-    fs.readFile(path.join(__dirname , 'data' , 'notes.txt'),'utf-8',(err,result) => {
-        if(err){
-            throw err;
-        }
-        fs.writeFile(path.join(__dirname , 'data','data.txt'),result,(err)=>{
-            if(err) throw err;
-            res.end(`Success`);
-        })
-        // res.end(`${result}`)
+    // fs.readFile(path.join(__dirname , 'data' , 'notes.txt'),'utf-8',(err,result) => {
+    //     if(err){
+    //         throw err;
+    //     }
+    //     fs.writeFile(path.join(__dirname , 'data','data.txt'),result,(err)=>{
+    //         if(err) throw err;
+    //         res.end(`Success`);
+    //     })
+    //     // res.end(`${result}`)
+    // });
+
+    fs.readFile(path.join(__dirname,'data','employees.json'),'utf-8',(err,result)=>{
+        if(err) throw err;
+        res.end(result);
     });
+
+
+
     // res.end(`${JSON.stringify(osData)}`);
     // res.end('<h3>welcome to node js rest api with ts</h3>');
 });
