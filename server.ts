a3b13fe27935b2b71432efcd491d8e1c88d00f25
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-07 15:29:42 +0530

    FS module

--- server.ts
+++ server.ts
@@ -1,22 +1,32 @@
 import http, { Server } from 'http';
 import os from 'os';
-
-
-
+import fs from 'fs';
+import path from 'path';
 
 const port = 4000;
 const host = '127.0.0.1';
 
 const server : Server = http.createServer((req,res)=>{
     res.statusCode=200;
+    res.setHeader('Content-Type','text/html');
     let osData = {
         totalMemory : os.totalmem(),
         freeMemory : os.freemem(),
         homedir : os.homedir(),
         computerName : os.hostname()
     };
-    res.end(`${JSON.stringify(osData)}`);
-    // res.setHeader('Content-Type','text/html');
+
+    fs.readFile(path.join(__dirname , 'data' , 'notes.txt'),'utf-8',(err,result) => {
+        if(err){
+            throw err;
+        }
+        fs.writeFile(path.join(__dirname , 'data','data.txt'),result,(err)=>{
+            if(err) throw err;
+            res.end(`Success`);
+        })
+        // res.end(`${result}`)
+    });
+    // res.end(`${JSON.stringify(osData)}`);
     // res.end('<h3>welcome to node js rest api with ts</h3>');
 });
 
