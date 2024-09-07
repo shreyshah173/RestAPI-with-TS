e25f20bd507dc403b288d5f711ae888ddcd25f10
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-07 15:16:23 +0530

    Basi of OS module

--- server.ts
+++ server.ts
@@ -1,11 +1,23 @@
 import http, { Server } from 'http';
+import os from 'os';
+
+
+
+
 const port = 4000;
 const host = '127.0.0.1';
 
 const server : Server = http.createServer((req,res)=>{
     res.statusCode=200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<h3>welcome to node js rest api with ts</h3>');
+    let osData = {
+        totalMemory : os.totalmem(),
+        freeMemory : os.freemem(),
+        homedir : os.homedir(),
+        computerName : os.hostname()
+    };
+    res.end(`${JSON.stringify(osData)}`);
+    // res.setHeader('Content-Type','text/html');
+    // res.end('<h3>welcome to node js rest api with ts</h3>');
 });
 
 server.listen(port,host, () => {
