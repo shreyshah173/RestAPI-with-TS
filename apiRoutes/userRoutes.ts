097dd82bbf1b034d680cd89c8dd4aeb562ac07e9
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-08 17:01:38 +0530

    Basic Bcrypt

--- apiRoutes/userRoutes.ts
+++ apiRoutes/userRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import apiMiddle from '../middlewares/apiMiddle';
+import bcrypt from 'bcryptjs';
 
 const userRouter : express.Router = express.Router();
 
@@ -8,13 +9,41 @@ userRouter.get('/' ,apiMiddle, (req,res) => {
 })
 
 
-userRouter.post('/login',(req,res)=>{
+userRouter.post('/login', async (req,res)=>{
     let formData = req.body;
-    res.status(200).json({
-        msg : "form data is recieved here",
-        formData : formData
-    })
+    try{
+        let salt = await bcrypt.genSalt(10);
+        let hash = await bcrypt.hash(formData.password, salt);
+
+        console.log("hash", hash);
+        res.status(200).json({
+            msg : "form data is recieved here",
+            formData : formData,
+            hasedPassword : hash
+        })
+
+        // if(formData.email && formData.password){
+        //     res.status(200).json({
+        //         msg : "form data is recieved here",
+        //         formData : formData
+        //     })
+        // }else{
+        //     res.status(400).json({
+        //         msg : "form data is not recieved here",
+        //         formData : formData
+        //     })
+        // }
+    }
+    catch(err){
+        console.log("error", err);
+    }
+    // res.status(200).json({
+    //     msg : "form data is recieved here",
+    //     formData : formData
+    // })
 })
+
+
 
 
 export default userRouter;
