8eccda6fc12d74782563b2a373a748681affd13d
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-08 17:17:38 +0530

    Form Validator

--- apiRoutes/userRoutes.ts
+++ apiRoutes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import apiMiddle from '../middlewares/apiMiddle';
 import bcrypt from 'bcryptjs';
+import { body, validationResult } from 'express-validator';
 
 const userRouter : express.Router = express.Router();
 
@@ -9,18 +10,28 @@ userRouter.get('/' ,apiMiddle, (req,res) => {
 })
 
 
-userRouter.post('/login', async (req,res)=>{
-    let formData = req.body;
-    try{
-        let salt = await bcrypt.genSalt(10);
-        let hash = await bcrypt.hash(formData.password, salt);
+userRouter.post('/login',
+    [
+    body('email').isEmail().withMessage('Email is not valid'),
+    body('password').isLength({min:5}).withMessage('Password should be atleast 5 characters long')
 
-        console.log("hash", hash);
-        res.status(200).json({
-            msg : "form data is recieved here",
-            formData : formData,
-            hasedPassword : hash
-        })
+    ] ,async (req:express.Request,res:express.Response)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+
+        let formData = req.body;
+        try{
+            let salt = await bcrypt.genSalt(10);
+            let hash = await bcrypt.hash(formData.password, salt);
+
+            console.log("hash", hash);
+            res.status(200).json({
+                msg : "form data is recieved here",
+                formData : formData,
+                hasedPassword : hash
+            })
 
         // if(formData.email && formData.password){
         //     res.status(200).json({
@@ -33,10 +44,10 @@ userRouter.post('/login', async (req,res)=>{
         //         formData : formData
         //     })
         // }
-    }
-    catch(err){
-        console.log("error", err);
-    }
+        }
+        catch(err){
+            console.log("error", err);
+        }
     // res.status(200).json({
     //     msg : "form data is recieved here",
     //     formData : formData
