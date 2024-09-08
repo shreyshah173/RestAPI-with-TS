648d587146812dd4dc8f61aa685622eabf285005
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-08 11:06:53 +0530

    POST method

--- server.ts
+++ server.ts
@@ -48,9 +48,23 @@ const host = '127.0.0.1';
 
 const server: Server = http.createServer((req,res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
+    res.setHeader('Content-Type','application/json');
 
-    ApiRoutes.mapRoutes(req,res);
+    // ApiRoutes.mapRoutes(req,res);
+    try{
+        if ( req.url ==='/users' && req.method==='POST'){
+            let body:any = '';
+            req.on('data' , (chunk) => {
+                body += chunk;
+            }).on('end' , () => {
+                let formatData = JSON.parse(body);
+                res.end(`${JSON.stringify(formatData)}`);
+            })
+        }
+    }
+    catch{
+        throw Error
+    }
 
 })
 
